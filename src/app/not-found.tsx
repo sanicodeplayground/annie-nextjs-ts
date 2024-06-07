b67e4a0e1fd7bf3c53c636f9b5ff0c6abac58f59
sanicodeplayground <magiur@gmail.com>
2024-06-07 12:53:39 +0100

    typo error fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,15 +3,9 @@ export default function NotFound() {
     <section>
       <div className="max-w-screen-md px-4 py-8 mx-auto lg:py-16 text-center">
         <h1 className="text-4xl font-extrabold mb-4">
-          Oh no! This page doesn't exist.
+          Oh no! This page doesn&apos;t exist.
         </h1>
-        <p>
-          If you expected to see something here, let me know{" "}
-          <a href="mailto&#58;a%&#54;&#69;%6&#69;ie&#98;ook&#101;ri&#108;lu%&#55;3tra&#116;&#105;on&#64;g%6Da%6&#57;%6C&#46;c%6&#70;m">
-            anni&#101;b&#111;&#111;&#107;erillustra&#116;i&#111;n&#64;gm&#97;il&#46;com
-          </a>
-          .
-        </p>
+        <p>If you expected to see something here, let me know.</p>
       </div>
     </section>
   );
